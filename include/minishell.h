9b51ed2aa110f76dee36b6f88908f194088296b1
Frederic Becerril <frederic.becerril@buzzee.fr>
2021-11-23 19:13:08 +0100

    add include

--- include/minishell.h
+++ include/minishell.h
@@ -9,4 +9,12 @@ typedef struct s_info
     char **env;
 } t_info;
 
+void    ft_cd(t_info *info);
+void    ft_echo(t_info *info);
+void    ft_env(t_info *info);
+void    ft_exit(t_info *info);
+void    ft_export(t_info *info);
+void    ft_pwd(t_info *info);
+void    ft_unset(t_info *info);
+
 #endif
